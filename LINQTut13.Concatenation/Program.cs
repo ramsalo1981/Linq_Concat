5efd059ad2baf657f13992f8cfaf86157d4b7769
ramsalo1981 <rami.salloum@hotmail.com>
2023-11-23 00:31:09 +0100

    first commit

---
+++ LINQTut13.Concatenation/Program.cs
@@ -0,0 +1,69 @@
+﻿
+using Shared;
+
+namespace LINQTut13.Concatenation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunMethod01();
+            RunMethod02();
+            RunMethod03();
+            RunMethod04();
+            Console.ReadKey();
+        }
+
+       
+
+        static void RunMethod01()
+        {
+            var quiz1 = QuestionBank.Randomize(3);
+            var quiz2 = QuestionBank.Randomize(2);
+
+            var quiz3 = quiz1.Concat(quiz2);
+            quiz3.ToQuiz();
+        }
+
+        static void RunMethod02()
+        {
+            var quiz1 = QuestionBank.Randomize(3);
+            var quiz2 = QuestionBank.Randomize(2);
+
+            //var questionTitles = quiz1.Select(q => q.Title).Concat(quiz2.Select(q => q.Title)).ToArray();
+            //questionTitles.ToList().ForEach(q => Console.WriteLine(q));
+
+            var questionTitles = quiz1.Select(q => q.Title)
+                .Concat(quiz2.Select(q => q.Title));
+
+
+            foreach (var title in questionTitles)
+                Console.WriteLine(title);
+
+        }
+
+        static void RunMethod03()
+        {
+            var questionTitles =
+                        QuestionBank.Randomize(3).Select(q => q.Title)
+                        .Concat(QuestionBank.Randomize(2).Select(q => q.Title))
+                        .Concat(QuestionBank.GetQuestionRange(Enumerable.Range(11, 14)).Select(q => q.Title));
+
+            foreach (var title in questionTitles)
+                Console.WriteLine(title);
+
+        }
+
+
+        static void RunMethod04()
+        {
+            var quiz1 = QuestionBank.Randomize(3);
+            var quiz2 = QuestionBank.Randomize(2);
+
+            var quiz3 = new[] { quiz1, quiz2 }.SelectMany(q => q);
+
+            quiz3.ToQuiz();
+        }
+
+    }
+}
---
+++ Shared/Question.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class Question
+    {
+        public string Title { get; set; }
+        public List<Choice> Choices { get; set; } = new();
+        public int CorrectAnswer { get; set; }
+
+
+        public override string ToString()
+        {
+            var choices = "";
+
+            foreach (var item in Choices)
+            {
+                choices += $"\n\t{item.Order}) {item.Description}";
+            }
+
+            return $"{Title}" +
+                   $"{choices}";
+
+
+        }
+    }
+}
